78fa86584de3109db1393b7f0031ece6714d52a6
Thomas Zwirner <tzwirne@gmx.de>
2024-02-10 12:36:43 +0100

    add callback on mount

--- src/index.js
+++ src/index.js
@@ -13,70 +13,78 @@ import React, {useState} from 'react'
  * @returns {JSX.Element}
  * @constructor
  */
-function Confirm_Dialog( args ) {
-	const [open, setOpen] = useState(true)
-
-	/**
-	 * Close action.
-	 */
-	const closeDialog = () => {
-		confirm_dialog.unmount();
-		confirm_dialog = null;
-		setOpen( false )
-	};
-
-	let classNames = "wp-easy-dialog";
-	if( args.dialog.className ) {
-		classNames = "wp-easy-dialog " + args.dialog.className;
+class Easy_Dialog extends React.Component {
+	componentDidMount() {
+		if( this.props.dialog.callback ) {
+			eval( this.props.dialog.callback );
+		}
 	}
 
 	/**
 	 * Output rendered dialog, with title, texts and buttons configured by JSON.
 	 *
 	 * Prepared possible button-actions:
-	 * closeDialog() => closes the dialog
+	 * - closeDialog() => closes the dialog
 	 */
-	return (
-		<div>
-		{open &&
-			<Modal
-				bodyOpenClassName="wp-easy-dialog-on-body"
-				className={classNames}
-				isDismissible={false}
-				onRequestClose={ closeDialog }
-				title={args.dialog.title}
-				shouldCloseOnClickOutside={false}
-				shouldCloseOnEsc={false}
-				__experimentalHideHeader={args.dialog.hide_title}
-			>
-				{args.dialog.texts && args.dialog.texts.map(function(text) {
+	render() {
+		let args = this.props;
+
+		/**
+		 * Define close action.
+		 */
+		const closeDialog = () => {
+			confirm_dialog.unmount();
+			confirm_dialog = null;
+		};
+
+		/**
+		 * Define class names.
+		 * @type {string}
+		 */
+		let classNames = "wp-easy-dialog";
+		if( this.props.dialog.className ) {
+			classNames = "wp-easy-dialog " + this.props.dialog.className;
+		}
+
+		return (
+			<div>
+				<Modal
+					bodyOpenClassName="wp-easy-dialog-on-body"
+					className={classNames}
+					isDismissible={false}
+					onRequestClose={ closeDialog }
+					title={args.dialog.title}
+					shouldCloseOnClickOutside={false}
+					shouldCloseOnEsc={false}
+					__experimentalHideHeader={args.dialog.hide_title}
+				>
+					{args.dialog.texts && args.dialog.texts.map(function(text) {
 							return (
 								<div key={text} dangerouslySetInnerHTML={{__html: text}} className="wp-easy-dialog-text" />
 							)
 						}
 					)
-				}
-				{args.dialog.progressbar && args.dialog.progressbar.active && (
-					<div
-						className="wp-progressbar"
-					>
-						<progress max="100" id={args.dialog.progressbar.id} value={args.dialog.progressbar.progress}>&nbsp;</progress>
-					</div>
-				)}
-				{args.dialog.buttons && args.dialog.buttons.map(function(button) {
-						return (
-							<Button key={button.text} variant={button.variant} onClick={ () => eval(button.action) }>
-								{button.text}
-							</Button>
-						)
 					}
-				)
-				}
-
-			</Modal>
-		}
-		</div>
-	);
+					{args.dialog.progressbar && args.dialog.progressbar.active && (
+						<div
+							className="wp-progressbar"
+						>
+							<progress max="100" id={args.dialog.progressbar.id} value={args.dialog.progressbar.progress}>&nbsp;</progress>
+						</div>
+					)}
+					{args.dialog.buttons && args.dialog.buttons.map(function(button) {
+							return (
+								<Button key={button.text} variant={button.variant} onClick={ () => eval(button.action) }>
+									{button.text}
+								</Button>
+							)
+						}
+					)
+					}
+				</Modal>
+			</div>
+		);
+	}
 }
 
 /**
@@ -100,7 +108,7 @@ function add_dialog( dialog ) {
 		}
 		confirm_dialog = ReactDOM.createRoot(document.getElementById('wp-easy-dialog-root'));
 		confirm_dialog.render(
-			<Confirm_Dialog dialog={dialog}/>
+			<Easy_Dialog dialog={dialog}/>
 		);
 	}
 }
