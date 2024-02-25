1a5e6e119ff4aa0f3af297d870e32cf584313563
Thomas Zwirner <tzwirne@gmx.de>
2024-02-25 11:39:44 +0100

    add optional label on progress

--- src/index.js
+++ src/index.js
@@ -8,75 +8,86 @@ import { Button, Modal } from '@wordpress/components';
 import React, {useState} from 'react'
 
 /**
- * Define the confirm dialog.
+ * Define the easy dialog modal for WordPress.
  *
  * @returns {JSX.Element}
  * @constructor
  */
-function Confirm_Dialog( args ) {
-	const [open, setOpen] = useState(true)
-
+class Easy_Dialog extends React.Component {
 	/**
-	 * Close action.
+	 * Run callback until component has been mount.
 	 */
-	const closeDialog = () => {
-		confirm_dialog.unmount();
-		confirm_dialog = null;
-		setOpen( false )
-	};
-
-	let classNames = "wp-easy-dialog";
-	if( args.dialog.className ) {
-		classNames = "wp-easy-dialog " + args.dialog.className;
+	componentDidMount() {
+		if( this.props.dialog.callback ) {
+			eval( this.props.dialog.callback );
+		}
 	}
 
 	/**
 	 * Output rendered dialog, with title, texts and buttons configured by JSON.
 	 *
 	 * Prepared possible button-actions:
-	 * closeDialog() => closes the dialog
+	 * - closeDialog() => closes the dialog
 	 */
-	return (
-		<div>
-		{open &&
+	render() {
+		let args = this.props;
+
+		/**
+		 * Define close action.
+		 */
+		const closeDialog = () => {
+			wp_easy_dialog.unmount();
+			wp_easy_dialog = null;
+		};
+
+		/**
+		 * Define class names.
+		 * @type {string}
+		 */
+		let classNames = "wp-easy-dialog";
+		if( this.props.dialog.className ) {
+			classNames = "wp-easy-dialog " + this.props.dialog.className;
+		}
+
+		return (
 			<Modal
 				bodyOpenClassName="wp-easy-dialog-on-body"
 				className={classNames}
 				isDismissible={false}
-				onRequestClose={ closeDialog }
+				onRequestClose={closeDialog}
 				title={args.dialog.title}
 				shouldCloseOnClickOutside={false}
 				shouldCloseOnEsc={false}
 				__experimentalHideHeader={args.dialog.hide_title}
 			>
-				{args.dialog.texts && args.dialog.texts.map(function(text) {
-							return (
-								<div key={text} dangerouslySetInnerHTML={{__html: text}} className="wp-easy-dialog-text" />
-							)
-						}
-					)
+				{args.dialog.texts && args.dialog.texts.map( function (text) {
+						return (
+							<div key={text} dangerouslySetInnerHTML={{__html: text}} className="wp-easy-dialog-text"/>
+						)
+					}
+				)
 				}
 				{args.dialog.progressbar && args.dialog.progressbar.active && (
 					<div
 						className="wp-progressbar"
 					>
-						<progress max="100" id={args.dialog.progressbar.id} value={args.dialog.progressbar.progress}>&nbsp;</progress>
+						<progress max="100" id={args.dialog.progressbar.id}
+								  value={args.dialog.progressbar.progress}>&nbsp;</progress>
+						{args.dialog.progressbar.label_id && <p id={args.dialog.progressbar.label_id}></p>}
 					</div>
 				)}
-				{args.dialog.buttons && args.dialog.buttons.map(function(button) {
+				{args.dialog.buttons && args.dialog.buttons.map( function (button) {
 						return (
-							<Button key={button.text} variant={button.variant} onClick={ () => eval(button.action) }>
+							<Button key={button.text} variant={button.variant} onClick={() => eval( button.action )}>
 								{button.text}
 							</Button>
 						)
 					}
 				)
 				}
-
 			</Modal>
-		}
-		</div>
-	);
+		)
+	}
 }
 
 /**
@@ -86,22 +97,37 @@ function Confirm_Dialog( args ) {
  *
  * @type {null}
  */
-let confirm_dialog = null;
-function add_dialog( dialog ) {
+let wp_easy_dialog = null;
+function add_easy_dialog( dialog ) {
 	if( dialog ) {
-		if ( confirm_dialog ) {
-			confirm_dialog.unmount();
-			confirm_dialog = null;
+		if ( wp_easy_dialog ) {
+			wp_easy_dialog.unmount();
+			wp_easy_dialog = null;
 		}
 		if( ! top.document.getElementById('wp-easy-dialog-root') ) {
 			let root = top.document.createElement('div');
 			root.id = 'wp-easy-dialog-root';
 			top.document.body.append(root);
 		}
-		confirm_dialog = ReactDOM.createRoot(document.getElementById('wp-easy-dialog-root'));
-		confirm_dialog.render(
-			<Confirm_Dialog dialog={dialog}/>
+		wp_easy_dialog = ReactDOM.createRoot( top.document.getElementById( 'wp-easy-dialog-root' ) );
+		wp_easy_dialog.render(
+			<Easy_Dialog dialog={dialog}/>
 		);
+	}
+}
+
+/**
+ * Initialize the Easy Dialog on every element with the class.
+ */
+function easy_dialog_init() {
+	let dialog_links = document.getElementsByClassName('wp-easy-dialog');
+	for( let i=0;i<dialog_links.length;i++ ) {
+		if( dialog_links[i].dataset.dialog ) {
+			dialog_links[i].onclick = function (e) {
+				e.preventDefault();
+				document.body.dispatchEvent( new CustomEvent( "wp-easy-dialog", { detail: JSON.parse( this.dataset.dialog ) } ) );
+			};
+		}
 	}
 }
 
@@ -112,17 +138,15 @@ document.addEventListener( 'DOMContentLoaded', () => {
 	// add listener which could be used to trigger the dialog with given configuration.
 	document.body.addEventListener('wp-easy-dialog', function(attr) {
 		if( attr.detail ) {
-			add_dialog(attr.detail);
+			add_easy_dialog(attr.detail);
 		}
 	});
 
-	// on each element with the class "wp-easy-dialog".
-	let dialog_links = document.getElementsByClassName('wp-easy-dialog');
-	for( let i=0;i<dialog_links.length;i++ ) {
-		dialog_links[i].onclick = function(e) {
-			e.preventDefault();
-			document.body.dispatchEvent(new CustomEvent("wp-easy-dialog", { detail: JSON.parse(this.dataset.dialog) }));
-		};
-	}
-})
+	// add listener for reinitialization.
+	document.body.addEventListener('wp-easy-dialog-reinit', function(attr) {
+		easy_dialog_init();
+	});
 
+	// on each element with the class "wp-easy-dialog".
+	easy_dialog_init();
+})
